4b81a6127dbf359687cb6c26d183e3253d6c5866
lkrids <lkrids@gmail.com>
2013-08-11 23:19:00 -0400

    complete overlay

--- www/js/www.js
+++ www/js/www.js
@@ -137,29 +137,101 @@
     return doT.template(tmpl, null, def);
   };
 }());
-;;(function($){
+;;(function($, window, document){
 
   "use strict";
 
-  var $noBtn = $(".jsNoBtn");
-  var $yesBtn = $(".jsYesBtn");
+  var $rsvpBtn = $(".jsRsvpBtn");
   var $overlay = $("#jsOverlay");
   var overlayTemplate = doT.template($("#jsRsvpConfirmation").html());
 
-  $noBtn.on({
+  $rsvpBtn.on({
+
     "click": function(e){
+
       e.preventDefault();
+
+      var data = { attend: $(this).data("attend")};
+      var html = overlayTemplate(data);
+      var $overlayContent = $overlay.find(".jsOverlayContent");
+
+      $overlay.data("attend", data.attend);
+
+      $overlayContent.html(html);
+
+      openOverlay($overlay);
+
     }
+
   });
 
-  $yesBtn.on({
+
+  $("body").on({
     "click": function(e){
+
       e.preventDefault();
+
+      closeOverlay($overlay);
+      
     }
+  }, ".jsCloseOverlay");
+
+
+  $(window).on({
+
+    keyup: function(e){
+      if (e.keyCode == 27) {
+        closeOverlay($overlay);
+      }
+    }
+
   });
 
-  function centerElement($ele){
-    
+
+  $overlay.on({
+    "click": function(e){
+
+      e.preventDefault();
+
+      var isAttending = $overlay.data("attend") === "won't" ? false : true;
+      console.log("is attending: "+isAttending);
+      //ajax data...
+      
+      closeOverlay($overlay);
+
+    }
+  }, ".jsConfirmationYes");
+
+
+  function closeOverlay($ele){
+
+    $ele.hide();
+    $ele.data("attend", "");
+
   }
 
-})(jQuery);
+
+  function openOverlay($ele){
+
+    $ele.show();
+    centerElement($ele.find(".jsOverlayContent"));
+
+  }
+
+
+  function centerElement($ele){
+
+    var halfOuterHeight = $($ele.selector).outerHeight(true) / 2;
+    var halfDocHeight = $(document).height() / 2;
+
+    var halfOuterWidth = $($ele.selector).outerWidth(true) / 2;
+    var halfDocWidth = $(document).width() / 2;
+
+    var topPx = halfDocHeight - halfOuterHeight || 0;
+    var leftPx = halfDocWidth - halfOuterWidth || 0;
+
+    $ele.css({ top: topPx, left: leftPx });
+
+  }
+
+})(jQuery, window, document);
